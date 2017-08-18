1e4e00561456087175accc8828c380f1e2e65b51
chenqing512 <chenqing512@sina.com>
2017-08-18 13:09:52 +0800

    Initial Commit

---
+++ CoreTextDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreTextDemo
+//
+//  Created by ChenQing on 17/8/18.
+//  Copyright © 2017年 ChenQing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreTextDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreTextDemo
+//
+//  Created by ChenQing on 17/8/18.
+//  Copyright © 2017年 ChenQing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
